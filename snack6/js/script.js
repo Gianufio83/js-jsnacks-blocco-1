9005f62109a6096515c33d67818a33854ef352c1
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-10 16:54:51 +0100

    rivisto snack6

--- snack6/js/script.js
+++ snack6/js/script.js
@@ -2,7 +2,34 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero
 // indicato dall’utente.
 
-var n = parseInt(prompt('inserisci un numero'));
-for (var i = 1; i <=n; i++) {
+
+
+var n = parseInt(prompt('Inserisci un numero'));
+console.log(n);
+for (var i = 1; i <= n; i++) {
   console.log(Math.pow(i, 3));
 }
+
+
+//  con il while
+
+// var n = parseInt(prompt('Inserisci un numero'));
+// console.log(n);
+// var i = 1 ;
+// while (i <= n) {
+//   console.log(Math.pow(i, 3));
+//   i++;
+// }
+
+
+
+
+
+
+
+
+
+// var n = parseInt(prompt('inserisci un numero'));
+// for (var i = 1; i <=n; i++) {
+//   console.log(Math.pow(i, 3));
+// }
