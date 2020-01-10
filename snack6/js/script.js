e89540badae7be0ca96c3364195f50bbc7c8447a
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-10 17:24:16 +0100

    aggiustamenti vari

--- snack6/js/script.js
+++ snack6/js/script.js
@@ -8,8 +8,12 @@ var n = parseInt(prompt('Inserisci un numero'));
 console.log(n);
 for (var i = 1; i <= n; i++) {
   console.log(Math.pow(i, 3));
+  var cubo = document.getElementById('numeri').innerHTML;
+  document.getElementById('numeri').innerHTML = cubo + Math.pow(i, 3);
+
 }
 
+document.getElementById('numeri').innerHTML = cubo + Math.pow(i, 3);
 
 //  con il while
 
